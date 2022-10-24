c93ed2cd1a8d0b6dd4f12ba43685f95361ffa302
RivnatZille <lucasvzille@hotmail.com>
2022-10-24 20:48:18 -0300

    Added custom routes and components for Error Handling

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,9 @@ import EventLayout from "@/views/event/EventLayout.vue";
 import EventDetails from "@/views/event/EventDetails.vue";
 import EventRegister from "@/views/event/EventRegister.vue";
 import EventEdit from "@/views/event/EventEdit.vue";
-import AboutView from "../views/AboutView.vue";
+import AboutView from "@/views/AboutView.vue";
+import NotFound from "@/views/NotFound.vue";
+import NetworkError from "@/views/NetworkError.vue";
 
 const routes = [
   {
@@ -53,6 +55,22 @@ const routes = [
     name: "About",
     component: AboutView,
     alias: "/about",
+  },
+  {
+    path: "/:catchAll(.*)",
+    name: "NotFound",
+    component: NotFound,
+  },
+  {
+    path: "/404/:resource",
+    name: "404Resource",
+    component: NotFound,
+    props: true,
+  },
+  {
+    path: "/network-error",
+    name: "NetworkError",
+    component: NetworkError,
   },
 ];
 
---
+++ src/views/NotFound.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Oops!</h1>
+  <h3>The {{ resource }} you're looking for isn't here.</h3>
+  <router-link :to="{ name: 'EventList' }">Back to the Home Page</router-link>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+
+  props: {
+    resource: {
+      type: String,
+      required: true,
+      default: "page",
+    },
+  },
+};
+</script>
+
+<style scoped></style>

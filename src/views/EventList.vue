c123d7fdee8718d09a7dd59aaf1c3c5fd8cda240
RivnatZille <lucasvzille@hotmail.com>
2022-10-25 20:58:19 -0300

    Worked on both Global and Per-Route Guards

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -28,7 +28,6 @@
 <script>
 import EventCard from "@/components/EventCard.vue";
 import EventService from "@/services/EventService.js";
-import NProgress from "nprogress";
 
 export default {
   name: "EventList",
@@ -47,7 +46,6 @@ export default {
   },
 
   beforeRouteEnter(routeTo, routeFrom, next) {
-    NProgress.start();
     EventService.getEvents(2, parseInt(routeTo.query.page) || 1)
       .then((response) => {
         next((comp) => {
@@ -57,24 +55,17 @@ export default {
       })
       .catch(() => {
         next({ name: "NetworkError" });
-      })
-      .finally(() => {
-        NProgress.done();
       });
   },
 
   beforeRouteUpdate(routeTo) {
-    NProgress.start();
-    EventService.getEvents(2, parseInt(routeTo.query.page) || 1)
+    return EventService.getEvents(2, parseInt(routeTo.query.page) || 1)
       .then((response) => {
         this.events = response.data;
         this.totalEvents = response.headers["x-total-count"];
       })
       .catch(() => {
         return { name: "NetworkError" };
-      })
-      .finally(() => {
-        NProgress.done();
       });
   },
 
